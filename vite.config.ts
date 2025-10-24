87aa2960378bb68b957dac6574b19e9094dcfb7f
touhid9pc <touhid@9point.capital>
2025-10-24 12:04:37 +0530

    favico

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,19 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 import svgr from "vite-plugin-svgr";
+import VitePluginSitemap from "vite-plugin-sitemap";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), svgr()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    svgr(),
+
+    VitePluginSitemap({
+      exclude: ["*"],
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
